6546b2335360e2a81dbe53b8e9323618a645f84a
unknown <aboubakrdatty@gmail.com>
2021-03-27 21:12:47 +0100

    projet test

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Contactez-nous</h1>
+@endsection
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+
+    <div>
+        <h1 class="d-flex justify-content-center">Mes produits</h1>
+    </div>
+    <div>
+        <div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="image">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+
+@endsection
